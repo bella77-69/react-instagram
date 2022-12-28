11d1494f09dbb29135055aa04c4c2100bdf37a74
chantelle mckenzie <mrspasceri@hotmail.com>
2022-12-27 21:15:52 -0800

    removed makeStyles from @mui/material/styles as it was not compatable with React18

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Post from "./components/post/Post";
 import "./App.css";
 import { db, auth } from "./firebase/FirebaseInit";
-import { makeStyles } from "@mui/material/styles";
+// import { makeStyles } from "@mui/material/styles";
 import Modal from "@mui/material/Modal";
 import ImageUpload from "./components/imageUpload/ImageUpload";
 
@@ -16,19 +16,19 @@ function getModalStyle() {
   }
 }
 
-const useStyles = makeStyles(() => ({
-  paper: {
-    position: "absolute",
-    width: 400,
-    backgroundColor: "white",
-    boxShadow: 24,
-    padding: "30px 60px",
-    borderRadius: "12px",
-  },
-}));
+// const useStyles = makeStyles(() => ({
+//   paper: {
+//     position: "absolute",
+//     width: 400,
+//     backgroundColor: "white",
+//     boxShadow: 24,
+//     padding: "30px 60px",
+//     borderRadius: "12px",
+//   },
+// }));
 
 function App() {
-  const classes = useStyles();
+  // const classes = useStyles();
   const [modalStyle] = useState(getModalStyle);
   const [posts, setPosts] = useState([]);
   const [openSignup, setOpenSignup] = useState(false);
@@ -89,7 +89,7 @@ function App() {
   return (
     <div className="app">
       <Modal open={openSignup} onClose={() => setOpenSignup(false)}>
-        <div style={modalStyle} className={classes.paper}>
+        <div>
           <center>
             <img
               src=" https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
@@ -128,7 +128,7 @@ function App() {
         </div>
       </Modal>
       <Modal open={openLogin} onClose={() => setOpenLogin(false)}>
-        <div style={modalStyle} className={classes.paper}>
+        <div>
           <center>
             <img
               src=" https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
